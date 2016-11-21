ba841e22264e1fb7ea050bcb5346a73fc2457d4f
gk <shanghai_sl@hotmail.com>
2016-11-21 13:00:37 +0800

    test

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello Angular!</h1>{{aaaxx | json}}'
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent { 
+export class AppComponent {
   constructor(private _http: Http) {
     this._http.get("https://raw.githubusercontent.com/shinhwagk/note-back-pages/data/index.json")
-    .map((res: Response) => res.json())
-    .toPromise().then(p=>this.aaaxx = p)
+      .map((res: Response) => res.json())
+      .toPromise().then(p => this._labels = p.labels)
   }
 
-  aaaxx
+  _labels
 }
