66d617fb6e3d00fe29c2f56d77887465ddeaed5b
gk <shanghai_sl@hotmail.com>
2016-11-22 16:24:59 +0800

    test

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,17 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { NoteComponent } from './note/note.component'
 import { OrclTabRelComponent } from './oracle-table-relation/otr.component';
 
-const routes_Note: Routes = [
+const routes: Routes = [
+  //note
   { path: '', redirectTo: '/note/index', pathMatch: 'full' },
-  { path: 'note/:path', component: NoteComponent }
-];
+  { path: 'note/:path', component: NoteComponent },
 
-const routes_OrclTabRel: Routes = [
+  //oracle table relation
   { path: 'orcltabrel/:name', component: OrclTabRelComponent }
 ];
-
-const routes = routes_note.concat(routes_orcl_tab_rel)
-
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
