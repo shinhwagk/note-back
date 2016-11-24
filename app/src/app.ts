ada0d9121368989877e9a7bb34b0ffd5076c53be
gk <shanghai_sl@hotmail.com>
2016-11-24 18:41:41 +0800

    save

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,13 @@
-console.info("test")
+import * as $ from 'jquery'
+$("#ABC").click(function () {
+    alert(111)
+});
+
+window["fff"] = function () {
+    alert(1111)
+}
+
+
+declare var cc = function(){
+    alert(222)
+}
