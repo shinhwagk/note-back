e5f569c13d98b9a0a8f02104efb06187a0c4b4eb
gk <shanghai_sl@hotmail.com>
2016-12-01 21:44:05 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ npm i --registry=https://registry.npm.taobao.org
 npm run webpack
 git log
 
+cd /
 git clone -b gh-pages https://github.com/shinhwagk/note-back gh-pages --depth=1
 
 rm -fr /gh-pages/*  
-mv /note-back/lib/* gh-pages/ 
+mv /note-back/lib/* /gh-pages/ 
 
 cd /gh-pages
 git add --all && git commit -m 'save'
