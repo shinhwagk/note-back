84b300f8562ee517de88f5c17bebf21cbc82c5fc
gk <shanghai_sl@hotmail.com>
2016-12-02 14:48:34 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 baseDir='/tmp/note-back-pages'
 
-if [[ -d $baseDir ]]; then rm -fr $baseDir; fi
+function rmBaseDir(){ if [[ -d $baseDir ]]; then rm -fr $baseDir; fi }
+
+rmBaseDir
 
 mkdir $baseDir && cd $baseDir && git clone https://github.com/shinhwagk/note-back note-back --depth=1 && cd ${baseDir}/note-back
 
 cp -r ~/.ssh . && docker build -t gk/note-back-pages --rm .
 
-rm -fr $baseDir
+rmBaseDir
