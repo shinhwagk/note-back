b355481e7ff5e09cd7af07d9cdc55f12de4bc0a4
gk <shanghai_sl@hotmail.com>
2016-12-02 14:30:14 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 bashDir='/tmp/note-back-pages'
 
+if [[ -d $bashDir ]]; then
+  rm -fr $bashDir
+fi
+
 mkdir $bashDir && cd $bashDir && git clone https://github.com/shinhwagk/note-back note-back --depth=1 && cd ${bashDir}/note-back
 
 cp -r ~/.ssh . && docker build -t gk/note-back-pages --rm .
