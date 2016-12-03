c99b1c1b25bdcf2516f6d48727790981314dec6a
gk <shanghai_sl@hotmail.com>
2016-12-03 16:53:24 +0800

    use jenkins

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-baseDir='/tmp/note-back-pages'
+baseDir="/tmp/note-back-pages"
 
-function rmBaseDir(){ if [[ -d $baseDir ]]; then rm -fr $baseDir; fi }
+rm -fr $baseDir
 
-rmBaseDir
+git clone https://github.com/shinhwagk/note-back $baseDir --depth=1
 
-mkdir $baseDir && cd $baseDir && git clone https://github.com/shinhwagk/note-back --depth=1 && cd ${baseDir}/note-back
-
-cp -r ~/.ssh . && docker build -t gk/note-back-pages --rm . && docker rmi $(docker images -f "dangling=true" -q)
-
-rmBaseDir
+cp -r ~/.ssh ./ && docker build -t gk/note-back-pages $baseDir
