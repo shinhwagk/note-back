a2aad2bc2c115df7289054c0e729ba426bfe1563
gk <shanghai_sl@hotmail.com>
2016-12-04 14:22:08 +0800

    use github pages docs folder for gh-pages

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-baseDir="/tmp/note-back-pages"
-
-rm -fr $baseDir
-
-git clone https://github.com/shinhwagk/note-back $baseDir --depth=1
-
-cp -r ~/.ssh $baseDir && docker build -t gk/note-back-pages $baseDir
