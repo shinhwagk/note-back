9109517a44e8bd269ac6ecae7f70a5fd3db94bbc
gk <shanghai_sl@hotmail.com>
2016-12-22 18:07:52 +0800

    add ps1

--- deploy.pages.ps1
+++ deploy.pages.ps1
@@ -1,7 +1,13 @@
-git clone -b gh-pages --depth=1 https://github.com/shinhwagk/note-back gh-pages
 ng build --prod
+git clone -b gh-pages --depth=1 https://github.com/shinhwagk/note-back gh-pages
+
+Get-ChildItem "dist" -Exclude "*.map", "*.gz" | Move-Item -Destination "gh-pages"
+
 cd gh-pages
 git add -A
 git commit -m "Update"
 git push
 cd ..
+
+Remove-Item -Force -Recurse "dist"
+Remove-Item -Force -Recurse "gh-pages"
