bce1b439343f7c8bde852333d6cea646a11740c7
shinhwagk <shanghai_sl@hotmail.com>
2016-12-22 17:53:32 +0800

    Update

--- deploy.pages.ps1
+++ deploy.pages.ps1
@@ -1,6 +1,5 @@
-Remove-Item -Recurse "gh-pages"
 git clone -b gh-pages --depth=1 https://github.com/shinhwagk/note-back gh-pages
-gh build --prod
+ng build --prod
 cd gh-pages
 git add -A
 git commit -m "Update"
